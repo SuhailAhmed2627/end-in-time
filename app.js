d3d65215d88e5cc78d09f9991e945239c09aa5ca
Suhail Ahmed <suhailahmed2001sam@gmail.com>
2021-10-10 23:10:52 +0530

    feat(all): Add GForm Support

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const messageDOM = document.getElementById("message");
+const hhDOM = document.getElementById("hh");
+const mmDOM = document.getElementById("mm");
+const ssDOM = document.getElementById("ss");
+const ampmDOM = document.getElementById("ampm");
+
 async function getCurrentTab() {
    let queryOptions = { active: true, currentWindow: true };
    let tab = await chrome.tabs.query(queryOptions);
@@ -6,7 +12,6 @@ async function getCurrentTab() {
 
 const injectScript = async () => {
    getCurrentTab().then(([tab]) => {
-      console.log(tab);
       chrome.scripting.executeScript({
          target: { tabId: tab.id },
          files: ["endInTime.js"],
@@ -15,8 +20,28 @@ const injectScript = async () => {
 };
 
 const setEndTime = () => {
-   console.log("emmememe");
-   injectScript();
+   if (hhDOM.value > 12 || mmDOM.value > 59 || ssDOM.value > 59) {
+      messageDOM.innerText = "Invalid Time";
+      return;
+   }
+
+   const now = new Date();
+   const endTime = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      +hhDOM.value + (ampmDOM.value == "pm" ? 12 : 0),
+      +mmDOM.value,
+      +ssDOM.value
+   );
+   if (endTime - Date.now() <= 2000) {
+      messageDOM.innerText = "Select Appropriate Time";
+      return;
+   }
+   chrome.storage.sync.set(
+      { endTime: JSON.stringify(endTime.getTime()) },
+      injectScript
+   );
 };
 
 document.getElementById("set").addEventListener("click", setEndTime);
--- endInTime.js
+++ endInTime.js
@@ -1,5 +1,25 @@
-[...document.querySelectorAll('[role="button"]')].forEach((div) => {
-   if (div.innerHTML.includes(">Submit</span>")) {
-      div.click();
+if (timeOuts == undefined) {
+   var timeOuts = [];
+}
+
+chrome.storage.sync.get(["endTime"], (result) => {
+   if (timeOuts) {
+      for (let i = 0; i < timeOuts.length; i++) {
+         clearTimeout(timeOuts[i]);
+      }
    }
+   const endTime = +result.endTime;
+   const submitForm = () => {
+      chrome.storage.sync.set({ endTime: "" });
+      [...document.querySelectorAll('[role="button"]')].forEach((div) => {
+         if (
+            div.innerHTML.includes(">Submit</span>") &&
+            Date.now() - endTime >= 0 &&
+            Date.now() - endTime <= 5000
+         ) {
+            div.click();
+         }
+      });
+   };
+   timeOuts.push(setTimeout(submitForm, endTime - Date.now()));
 });
